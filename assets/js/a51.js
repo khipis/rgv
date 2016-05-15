daf20dc9510179e50c16b6b2bc7c9357cf5929b3
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2016-05-15 11:25:04 +0200

    Port generators from c to js

--- assets/js/a51.js
+++ assets/js/a51.js
@@ -1,15 +1,17 @@
-#include <stdio.h>
+var REG_1_COUNT = 19;
+var REG_2_COUNT = 22;
+var REG_3_COUNT = 23;
+var KEY_COUNT = 64;
 
-#define REG_1_COUNT 19
-#define REG_2_COUNT 22
-#define REG_3_COUNT 23
-#define KEY_COUNT 64
+var reg1
+[REG_1_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+var reg2
+[REG_2_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+var reg3
+[REG_3_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-int reg1[REG_1_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int reg2[REG_2_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int reg3[REG_3_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-int key[KEY_COUNT] = {
+var key =
+[KEY_COUNT] = {
     1, 0, 0, 0,
     0, 0, 0, 0,
     0, 0, 0, 0,
@@ -29,30 +31,18 @@ int key[KEY_COUNT] = {
 };
 
 
-void a51_init();
-
-int lfsr_1();
-
-int lfsr_2();
-
-int lfsr_3();
-
-int a51();
-
-
-int main(int argc, char **argv) {
+function main(){
 
     a51_init();
 
-    int i = 0;
+    var i = 0;
     for (i = 0; i < 10; i++) {
-        putchar(a51() + 48);
+ /**/       putchar(a51() + 48);
     }
 }
 
-void a51_init() {
-    int i = 0;
-    for (i = 0; i < KEY_COUNT; i++) {
+function a51_init() {
+    for (var i = 0; i < KEY_COUNT; i++) {
         reg1[0] = key[i] ^ reg1[0];
         reg2[0] = key[i] ^ reg2[0];
         reg3[0] = key[i] ^ reg3[0];
@@ -62,44 +52,49 @@ void a51_init() {
         lfsr_3();
     }
 
-    for (i = 0; i < 100; i++) {
+    for (var i = 0; i < 100; i++) {
         a51();
     }
 
 }
 
-int lfsr_1() {
-    int temp = reg1[18] ^reg1[17] ^reg1[16] ^reg1[13];   //1+x14+x17+x18+x19
-    int k = 0;
-    for (k = REG_1_COUNT - 1; k > 0; k--) reg1[k] = reg1[k - 1];
+function lfsr_1() {
+    var temp = reg1[18] ^ reg1[17] ^ reg1[16] ^ reg1[13];   //1+x14+x17+x18+x19
+    for (var k = REG_1_COUNT - 1; k > 0; k--) {
+        reg1[k] = reg1[k - 1];
+    }
     reg1[0] = temp;
     return reg1[18];
 }
 
-int lfsr_2() {
-    int temp = reg2[21] ^reg2[20]; //x22+x21+1
-    int k = 0;
-    for (k = REG_2_COUNT - 1; k > 0; k--) reg2[k] = reg2[k - 1];
+function lfsr_2() {
+    var temp = reg2[21] ^ reg2[20]; //x22+x21+1
+    var k = 0;
+    for (k = REG_2_COUNT - 1; k > 0; k--) {
+        reg2[k] = reg2[k - 1];
+    }
     reg2[0] = temp;
     return reg2[21];
 }
 
-int lfsr_3() {
-    int temp = reg3[22] ^reg3[21] ^reg3[20] ^reg3[7];   //1+x8+x21+x22+x23
-    int k = 0;
-    for (k = REG_3_COUNT - 1; k > 0; k--) reg3[k] = reg3[k - 1];
+function lfsr_3() {
+    var temp = reg3[22] ^ reg3[21] ^ reg3[20] ^ reg3[7];   //1+x8+x21+x22+x23
+    var k = 0;
+    for (k = REG_3_COUNT - 1; k > 0; k--) {
+        reg3[k] = reg3[k - 1];
+    }
     reg3[0] = temp;
     return reg3[22];
 }
 
-int a51() {
-    int clock1 = reg1[8];
-    int clock2 = reg2[10];
-    int clock3 = reg3[10];
+function a51() {
+    var clock1 = reg1[8];
+    var clock2 = reg2[10];
+    var clock3 = reg3[10];
 
-    int lf1 = reg1[18]; //Old bit value
-    int lf2 = reg2[21]; //Old bit value
-    int lf3 = reg3[22]; //Old bit value
+    var lf1 = reg1[18]; //Old bit value
+    var lf2 = reg2[21]; //Old bit value
+    var lf3 = reg3[22]; //Old bit value
 
     if (clock1 == clock2 || clock1 == clock3) {
         lfsr_1();
--- assets/js/generators.js
+++ assets/js/generators.js
@@ -1,59 +1,71 @@
-bool stage[3] = {true, true, true};
+var stage
+[3] = {true, true, true};
 
-int reg1[20] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int reg2[20] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-int reg3[20] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+var reg1
+[20] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+var reg2
+[20] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+var reg3
+[20] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-int lfsr_1() {
+function lfsr_1() {
     if (stage[0]) {
         stage[0] = false;
         return reg1[0];
     }
-    int n = 18, temp = reg1[6] ^reg1[17];   //1+x7+x18
-    int k = 0;
-    for (k = n - 1; k > 0; k--) reg1[k] = reg1[k - 1];
+    var n = 18, temp = reg1[6] ^ reg1[17];   //1+x7+x18
+    var k = 0;
+    for (k = n - 1; k > 0; k--) {
+        reg1[k] = reg1[k - 1];
+    }
     reg1[0] = temp;
     return temp;
 }
 
-int lfsr_2() {
+function lfsr_2() {
     if (stage[1]) {
         stage[1] = false;
         return reg2[0];
     }
-    int n = 19, temp = reg2[0] ^reg2[1] ^reg2[4] ^reg2[18]; //1+x+x2+x5+x19
-    int k = 0;
-    for (k = n - 1; k > 0; k--) reg2[k] = reg2[k - 1];
+    var n = 19, temp = reg2[0] ^ reg2[1] ^ reg2[4] ^ reg2[18]; //1+x+x2+x5+x19
+    var k = 0;
+    for (k = n - 1; k > 0; k--) {
+        reg2[k] = reg2[k - 1];
+    }
     reg2[0] = temp;
     return temp;
 }
 
-int lfsr_3() {
+function lfsr_3() {
     if (stage[2]) {
         stage[2] = false;
         return reg3[0];
     }
-    int n = 20, temp = reg3[2] ^reg3[19];   //1+x3+x20
-    int k = 0;
-    for (k = n - 1; k > 0; k--) reg3[k] = reg3[k - 1];
+    var n = 20, temp = reg3[2] ^ reg3[19];   //1+x3+x20
+    var k = 0;
+    for (k = n - 1; k > 0; k--) {
+        reg3[k] = reg3[k - 1];
+    }
     reg3[0] = temp;
     return temp;
 }
 
-
-int geffe() {
-    int lf1 = lfsr_1();
-    int lf2 = lfsr_2();
-    int lf3 = lfsr_3();
-    int and1 = lf1 & lf2;
-    int and2;
-    if (lf2 == 1) and2 = 0 & lf3;
-    else and2 = 1 & lf3;
+function geffe() {
+    var lf1 = lfsr_1();
+    var lf2 = lfsr_2();
+    var lf3 = lfsr_3();
+    var and1 = lf1 & lf2;
+    var and2;
+    if (lf2 == 1) {
+        and2 = 0 & lf3;
+    } else {
+        and2 = 1 & lf3;
+    }
     return and1 ^ and2;
 }
 
-int stop_and_go() {
-    int lf2, lf3;
+function stop_and_go() {
+    var lf2, lf3;
     if (lfsr_1() == 1) {
         lf2 = lfsr_2();
         lf3 = reg3[0];
@@ -65,9 +77,9 @@ int stop_and_go() {
     return lf2 ^ lf3;
 }
 
-int shrinker() {
-    int lf1 = lfsr_1();
-    int lf2 = lfsr_2();
+function shrinker() {
+    var lf1 = lfsr_1();
+    var lf2 = lfsr_2();
 
     while (lf1 != 1) {
         lf1 = lfsr_1();
@@ -76,8 +88,7 @@ int shrinker() {
     return lf2;
 }
 
-int i = 0;
-for (i = 0; i < 100; i++) {
-    putchar(geffe() + 48);
-}
+// for (var i = 0; i < 100; i++) {
+//     putchar(geffe() + 48);
+// }
 
