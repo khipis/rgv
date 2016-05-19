35403241f0a8e3b067b62423fb017adcc8c1b4eb
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2016-05-19 12:15:48 +0200

    Add ability to change lights intensity

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -3,7 +3,15 @@ if (BABYLON.Engine.isSupported()) {
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
     var camera = setupCamera();
-/*    var sun = new BABYLON.PointLight("sun", new BABYLON.Vector3(50, 50, 30), scene);*/
+
+    var options ={
+        intensity : 0.30,
+        lights : true,
+        generator : 'Shrinker'
+    };
+
+    var sun = new BABYLON.PointLight("sun", new BABYLON.Vector3(50, 50, 30), scene);
+    sun.intensity = options.intensity;
 
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
@@ -15,12 +23,7 @@ if (BABYLON.Engine.isSupported()) {
         scene.render();
     });
 
-    var options ={
-        seed : 0.30,
-        cloudSeed : 0.60,
-        clouds : false,
-        generator : 'Shrinker'
-    };
+
 
  /*   addSkybox();*/
     addSpheres(100);
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -4,12 +4,12 @@ gui.add(options, 'generator', ['Shrinker', 'Geffe', 'Stop and go', 'A5/1']).onCh
     options.generator = value;
 });
 
-gui.add(options, 'clouds').onChange(function (value) {
-    options.clouds = value
+gui.add(options, 'lights').onChange(function (value) {
+    sun.isEnabled = value;
 });
 
-gui.add(options, 'seed', 0.1, 1.0).onFinishChange(function () {
-    options.seed = value;
+gui.add(options, 'intensity', 0.0, 5.0).onFinishChange(function (value) {
+    sun.intensity = value;
 });
 
 
